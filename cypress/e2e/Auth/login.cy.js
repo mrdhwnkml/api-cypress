06f0ee03ac73df9549d0f9a5b59df2a131c1bcc1
M Ridhwan K <mrdhwnkml@gmail.com>
2026-06-15 11:16:33 +0700

    fix logic login

--- cypress/e2e/Auth/login.cy.js
+++ cypress/e2e/Auth/login.cy.js
@@ -1,31 +1,16 @@
 /// <reference types ='Cypress'/>
+import { loadToken } from '../../support/Auth'
 const faker = require('faker')
+
 describe('User Login', () => {
-  let signUpData
-
-  before(() => {
-    cy.fixture('data').then((data) => {
-      signUpData = data
+    let accessToken
+    before(() => {
+      loadToken().then((token) => {
+        accessToken = token
+      })
     })
-  })
   it('Login user with valid data should be success', () => {
-    cy.api({
-      method: 'POST',
-      url: 'api/login',
-      failOnStatusCode: false,
-      body: {
-        username: signUpData.email,
-        password: signUpData.password,
-      },
-    }).then((response) => {
-      expect(response.status).to.be.equal(200)
-      expect(response.body).to.have.property('Authorization')
-      expect(response.body.success).to.equal(signUpData.email + ' ' + 'is now logged in')
-      cy.log(response.body.Authorization)
+  expect(accessToken).to.be.a('string')
 
-      var token = response.body.Authorization
-      localStorage.setItem('token', token)
-      // localStorage.getItem('id')
-    })
   })
 })
