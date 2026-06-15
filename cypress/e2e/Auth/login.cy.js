b314e63ec20ac5bba0ed41fbe3b98c5c8bd1d549
M Ridhwan K <mrdhwnkml@gmail.com>
2026-06-15 11:16:53 +0700

    fix logic login

--- cypress/e2e/Auth/login.cy.js
+++ cypress/e2e/Auth/login.cy.js
@@ -3,14 +3,13 @@ import { loadToken } from '../../support/Auth'
 const faker = require('faker')
 
 describe('User Login', () => {
-    let accessToken
-    before(() => {
-      loadToken().then((token) => {
-        accessToken = token
-      })
+  let accessToken
+  before(() => {
+    loadToken().then((token) => {
+      accessToken = token
     })
+  })
   it('Login user with valid data should be success', () => {
-  expect(accessToken).to.be.a('string')
-
+    expect(accessToken).to.be.a('string')
   })
 })
